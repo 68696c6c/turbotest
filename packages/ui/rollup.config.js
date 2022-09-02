692ce26cab2d1ba4b004fb6cdac49505c19f5119
Aaron Hill <aaron.hill@shift.com>
2022-09-02 10:37:34 -0600

    Initial commit.

---
+++ packages/ui/rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from '@rollup/plugin-typescript'
+import del from 'rollup-plugin-delete'
+import { defineConfig } from 'rollup'
+
+const config = defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: 'dist',
+      preserveModules: true,
+      sourcemap: true,
+    },
+  ],
+  external: [
+    'react',
+    '@radix-ui/react-dialog',
+    '@radix-ui/react-visually-hidden',
+    '@stitches/react',
+    'react/jsx-runtime',
+    'react/jsx-dev-runtime',
+    'lodash.without',
+    'uuid',
+    '@radix-ui/react-progress',
+  ],
+  plugins: [del({ targets: 'dist/*' }), typescript()],
+})
+
+export default config
